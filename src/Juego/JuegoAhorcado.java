177b82efe0ce0ed275d8207a65035d07ba0c7b33
JAVIER LEGUA <javierlegua14@gmail.com>
2020-11-30 11:51:12 +0100

    Llamada a metodos alojados en otro archivo

--- src/Juego/JuegoAhorcado.java
+++ src/Juego/JuegoAhorcado.java
@@ -4,226 +4,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoAhorcado {
-	// Pinta el muñeco en pantalla dependiendo de las vidas
-	public static void dibujarMuneco(int vidas) {
-		switch (vidas) {
-		case 7:
-			System.out.println("");
-			break;
-		case 6:
-			System.out.println("______________________");
 
-			break;
-		case 5:
-			System.out.println("___________");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|______________________");
-
-			break;
-		case 4:
-			System.out.println("___________");
-			System.out.println("|          _|_");
-			System.out.println("|         | oo|");
-			System.out.println("|         |__/| ");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|______________");
-
-			break;
-		case 3:
-			System.out.println("__________");
-			System.out.println("|        _|_");
-			System.out.println("|        | oo|");
-			System.out.println("|        |_ /| ");
-			System.out.println("|          |");
-			System.out.println("|          |");
-			System.out.println("|          |");
-			System.out.println("|          |");
-			System.out.println("|			   		");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|______________");
-
-			break;
-		case 2:
-			System.out.println("__________");
-			System.out.println("|        _|_");
-			System.out.println("|        | oo|");
-			System.out.println("|        |_ /| ");
-			System.out.println("|          |");
-			System.out.println("|     -----|-----");
-			System.out.println("|          |");
-			System.out.println("|          |");
-			System.out.println("|			   		");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|");
-			System.out.println("|______________");
-
-
-			break;
-		case 1:
-			System.out.println("__________");
-			System.out.println("|        _|_");
-			System.out.println("|        | xx|");
-			System.out.println("|        |_ /| ");
-			System.out.println("|          |");
-			System.out.println("|     -----|-----");
-			System.out.println("|          |");
-			System.out.println("|          |");
-			System.out.println("|        /   \\");
-			System.out.println("|       /     \\");
-			System.out.println("|      /       \\");
-			System.out.println("|     /         \\");
-			System.out.println("|______________");
-
-
-			break;
-		case 0:
-
-			break;
-		}
-	}
-
-	// Comprueba que una letra existe en la palabra.
-	// Devuelve verdadero si se encuentra, falso en caso contrario
-	public static boolean comprobarLetraPalabra(String vPalabraSecreta[], String letra) {
-		boolean encontrado = false;
-
-		return encontrado;
-	}
-
-	public static void inicializarVectores(String palabra, String vPalabraSecreta[], String vAciertos[],
-			String vFallos[]) {
-		for (int i = 0; i < vFallos.length; i++) {
-			vFallos[i] = "_";
-		}
-		for (int i = 0; i < vPalabraSecreta.length; i++) {
-			// Trocear la palabra en letras al vector
-			vPalabraSecreta[i] = palabra.substring(i, i + 1);
-			vAciertos[i] = "_";
-		}
-
-	}
-
-	public static void dibujarAciertorErrores(String[] vFallos, String[] vAciertos) {
-
-		// Imprimir los fallos
-		System.out.println("Fallos cometidos:");
-		for (int i = 0; i < vFallos.length; i++) {
-			if (!vFallos[i].equals("_")) {
-				System.out.print(vFallos[i] + " ");
-			}
-		}
-
-		// System.out.println("");
-		System.out.println("\n¡Palabra Secreta!");
-		// Imprimir los aciertos
-		for (int i = 0; i < vAciertos.length; i++) {
-			System.out.print(vAciertos[i] + " ");
-		} 
-		System.out.println("");
-	}
-
-	// Comprobar que la letra está en vPalabraSecreta
-	// Si esta la guardo en vAciertos, sino la guardo en vFallos
-	public static int comprobarLetraIntroducida(int vidas, String letra, String[] vPalabraSecreta, String[] vAciertos,
-			String[] vFallos) {
-		boolean encontrado = false;
-
-		for (int i = 0; i < vPalabraSecreta.length; i++) {
-			if (letra.equalsIgnoreCase(vPalabraSecreta[i])) {
-				vAciertos[i] = letra;
-				encontrado = true;
-			}
-		}
-
-		if (encontrado == false) {
-			for (int i = 0; i < vFallos.length; i++) {
-				if (vFallos[i].equals("_")) {
-					vFallos[i] = letra;
-					vidas--;
-					break;
-				}
-			}
-		}
-
-		return vidas;
-	}
-
-	//Cambio
-	public static boolean heGanado(String vAciertos[]) {
-
-		//boolean ganado = false;
-		
-		for (int i = 0; i < vAciertos.length; i++) {
-			if (vAciertos[i].equals("_")) {
-				return false;
-				//ganado=false;
-				//break;
-			}
-		}
-		//return ganado;
-		return true;
-	}
-	
-	public static String generarPalabra() {
-		String palabra;
-		int num;
-		String vPalabraSecreta[] = {"hola", "adios", "mañana", "ayer", "javier", "esternocleidomastoideo", "juanfran", "kacper", "informatica", "alloza"};
-		Random r = new Random();
-		num = r.nextInt(vPalabraSecreta.length);
-		palabra=vPalabraSecreta[num];
-		return palabra;
-	}
-	
 	public static void main(String[] args) {
 		// Vidas totales 7
 		Scanner leer = new Scanner(System.in);
-		int vidas = 7 ;
-		String palabraSecreta = generarPalabra();
+		int vidas = 7;
+		String palabraSecreta = metodos.generarPalabra();
 		String letra;
 		String vAciertos[], vFallos[];
 		String vPalabraSecreta[] = new String[palabraSecreta.length()];
 		vAciertos = new String[palabraSecreta.length()];
-		vFallos = new String[vidas+1];
-		inicializarVectores(palabraSecreta, vPalabraSecreta, vAciertos, vFallos);
+		vFallos = new String[vidas + 1];
+		metodos.inicializarVectores(palabraSecreta, vPalabraSecreta, vAciertos, vFallos);
 		// Estructura general del juego
 		do {
 			// 1º Preguntar letra
 			System.out.println("Dime una letra");
 			letra = leer.next();
 			// 2º Comprobar si la letra está en la palabra
-			vidas = comprobarLetraIntroducida(vidas, letra, vPalabraSecreta, vAciertos, vFallos);
+			vidas = metodos.comprobarLetraIntroducida(vidas, letra, vPalabraSecreta, vAciertos, vFallos);
 
 			// 3º Dibujar muñeco
-			dibujarMuneco(vidas);
+			metodos.dibujarMuneco(vidas);
 			// 4º Dibujar aciertos y errores
-			dibujarAciertorErrores(vFallos, vAciertos);
+			metodos.dibujarAciertorErrores(vFallos, vAciertos);
 
-		} while (vidas > 1 && heGanado(vAciertos) == false);
-			System.out.println("");
-			if (vidas>1)
-				System.out.println("GANASTE!!!!!!");
-			else
-				System.out.println("Has perdido");
+		} while (vidas > 1 && metodos.heGanado(vAciertos) == false);
+		System.out.println("");
+		if (vidas > 1)
+			System.out.println("GANASTE!!!!!!");
+		else
+			System.out.println("Has perdido");
 	}
 
 }
---
+++ src/Juego/metodos.java
@@ -0,0 +1,197 @@
+package Juego;
+
+import java.util.Random;
+
+public class metodos {
+
+	// Pinta el muñeco en pantalla dependiendo de las vidas
+	public static void dibujarMuneco(int vidas) {
+		switch (vidas) {
+		case 7:
+			System.out.println("");
+			break;
+		case 6:
+			System.out.println("______________________");
+
+			break;
+		case 5:
+			System.out.println("___________");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|______________________");
+
+			break;
+		case 4:
+			System.out.println("___________");
+			System.out.println("|          _|_");
+			System.out.println("|         | oo|");
+			System.out.println("|         |__/| ");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|______________");
+
+			break;
+		case 3:
+			System.out.println("__________");
+			System.out.println("|        _|_");
+			System.out.println("|        | oo|");
+			System.out.println("|        |_ /| ");
+			System.out.println("|          |");
+			System.out.println("|          |");
+			System.out.println("|          |");
+			System.out.println("|          |");
+			System.out.println("|			   		");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|______________");
+
+			break;
+		case 2:
+			System.out.println("__________");
+			System.out.println("|        _|_");
+			System.out.println("|        | oo|");
+			System.out.println("|        |_ /| ");
+			System.out.println("|          |");
+			System.out.println("|     -----|-----");
+			System.out.println("|          |");
+			System.out.println("|          |");
+			System.out.println("|			   		");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|______________");
+
+			break;
+		case 1:
+			System.out.println("__________");
+			System.out.println("|        _|_");
+			System.out.println("|        | xx|");
+			System.out.println("|        |_ /| ");
+			System.out.println("|          |");
+			System.out.println("|     -----|-----");
+			System.out.println("|          |");
+			System.out.println("|          |");
+			System.out.println("|        /   \\");
+			System.out.println("|       /     \\");
+			System.out.println("|      /       \\");
+			System.out.println("|     /         \\");
+			System.out.println("|______________");
+
+			break;
+		case 0:
+
+			break;
+		}
+	}
+
+	// Comprueba que una letra existe en la palabra.
+	// Devuelve verdadero si se encuentra, falso en caso contrario
+	public static boolean comprobarLetraPalabra(String vPalabraSecreta[], String letra) {
+		boolean encontrado = false;
+
+		return encontrado;
+	}
+
+	public static void inicializarVectores(String palabra, String vPalabraSecreta[], String vAciertos[],
+			String vFallos[]) {
+		for (int i = 0; i < vFallos.length; i++) {
+			vFallos[i] = "_";
+		}
+		for (int i = 0; i < vPalabraSecreta.length; i++) {
+			// Trocear la palabra en letras al vector
+			vPalabraSecreta[i] = palabra.substring(i, i + 1);
+			vAciertos[i] = "_";
+		}
+
+	}
+
+	public static void dibujarAciertorErrores(String[] vFallos, String[] vAciertos) {
+
+		// Imprimir los fallos
+		System.out.println("Fallos cometidos:");
+		for (int i = 0; i < vFallos.length; i++) {
+			if (!vFallos[i].equals("_")) {
+				System.out.print(vFallos[i] + " ");
+			}
+		}
+
+		// System.out.println("");
+		System.out.println("\n¡Palabra Secreta!");
+		// Imprimir los aciertos
+		for (int i = 0; i < vAciertos.length; i++) {
+			System.out.print(vAciertos[i] + " ");
+		}
+		System.out.println("");
+	}
+
+	// Comprobar que la letra está en vPalabraSecreta
+	// Si esta la guardo en vAciertos, sino la guardo en vFallos
+	public static int comprobarLetraIntroducida(int vidas, String letra, String[] vPalabraSecreta, String[] vAciertos,
+			String[] vFallos) {
+		boolean encontrado = false;
+
+		for (int i = 0; i < vPalabraSecreta.length; i++) {
+			if (letra.equalsIgnoreCase(vPalabraSecreta[i])) {
+				vAciertos[i] = letra;
+				encontrado = true;
+			}
+		}
+
+		if (encontrado == false) {
+			for (int i = 0; i < vFallos.length; i++) {
+				if (vFallos[i].equals("_")) {
+					vFallos[i] = letra;
+					vidas--;
+					break;
+				}
+			}
+		}
+
+		return vidas;
+	}
+
+	// Cambio
+	public static boolean heGanado(String vAciertos[]) {
+
+		// boolean ganado = false;
+
+		for (int i = 0; i < vAciertos.length; i++) {
+			if (vAciertos[i].equals("_")) {
+				return false;
+				// ganado=false;
+				// break;
+			}
+		}
+		// return ganado;
+		return true;
+	}
+
+	public static String generarPalabra() {
+		String palabra;
+		int num;
+		String vPalabraSecreta[] = { "hola", "adios", "mañana", "ayer", "javier", "esternocleidomastoideo", "juanfran",
+				"kacper", "informatica", "alloza" };
+		Random r = new Random();
+		num = r.nextInt(vPalabraSecreta.length);
+		palabra = vPalabraSecreta[num];
+		return palabra;
+	}
+
+	
+}
